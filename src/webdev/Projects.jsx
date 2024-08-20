ae09568e9e9dc8a07cdef9b1f2458d9289eb6e54
Ben Aguirre <baguirre@luc.edu>
2024-08-20 14:38:03 -0400

    Name update

--- src/webdev/Projects.jsx
+++ src/webdev/Projects.jsx
@@ -37,7 +37,7 @@ const projects = [
       },
       { title: "Live", src: "https://members-only-top.fly.dev/" },
     ],
-    skills: ["Node.JS", "MongoDB", "Express", "Passport.js", "Pug", "CSS"],
+    skills: ["Node.js", "MongoDB", "Express", "Passport.js", "Pug", "CSS"],
     src: members,
   },
   {
